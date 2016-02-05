8d8888b01ce1c4030d465fbbd46eca7472573fb6
James Reed <cisprofjrr@alumni.ohio.edu>
2016-02-05 10:22:22 -0500

    rev2BacktoC.c
    Re-engineered back to C code--not ROBOTC.

---
+++ rev2ROBOTC2c.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+//shell program converted from ROBOTC for Professor Reed's -- OETC Robotic Workshop 2016
+//
+//Reversed engineering of this program for EV3 and back to C_language
+//
+//Note: this program will NOT run on the EV3
+//      but is presented here to demonstrate
+//      practical progam design ONLY!
+//
+//#include <stdio.h>
+int myval1 = 1; // defined as global
+//
+//
+//subroutines follow: (must be before task main() or int main() in regular c
+//
+//============================================
+//do inputs
+//============================================
+//
+void inputs(void)
+{
+	//myval1 = 1;
+               printf("\n inputs routine has started");
+               printf("\n myval1 is: ");
+               printf("%d\n",myval1);
+            //printf("myval1);
+    return;
+}
+// 
+//============================================
+//do processing
+//============================================
+//
+void processing(int myval2)
+{
+               printf("\nprocessing routine has started");
+               printf("\n myval2 is: ");
+               printf("%d\n",myval2);
+               return;
+               
+}
+//
+//============================================
+//do outputs
+//*** start here for designing a new project
+//============================================
+//
+void outputs(int myval3)
+{
+               printf("\noutputs routine has started");
+               printf("\nprocessing routine has started");
+               printf("\n myval3 is: ");
+               printf("%d\n",myval3);
+               return;
+}
+//
+
+
+//task main()
+int main(void)
+{
+               printf("start of int main()");
+               // your code goes here
+
+
+               int myval2 = 2; // defined as local to task main()(preferred)
+               int myval3 = 3;
+               
+               inputs();
+               processing(myval2);
+               outputs(myval3);
+   
+               printf("end of int main()");
+
+
+               return 0;
+} // end of int main()
